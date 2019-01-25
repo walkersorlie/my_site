3da4675c3bd538d480b2d1e683b65e3c4c2e071a
Walker Sorlie <walkersorlie@gmail.com>
2019-01-25 14:35:17 +0100

    Fixed timezone aware problem. Changed Django TZ to 'UTC'

--- my_site/homepage/management/commands/repos_info.py
+++ my_site/homepage/management/commands/repos_info.py
@@ -3,9 +3,9 @@ import json as json_py
 import os
 from django.core.management.base import BaseCommand, CommandError
 from django.core.exceptions import ImproperlyConfigured
+from django.utils import timezone
 from datetime import datetime
 from homepage.models import Repository
-import sys
 
 
 class Command(BaseCommand):
@@ -30,33 +30,27 @@ class Command(BaseCommand):
         # print (r.text)
         # print (json_py.dumps(r.text, sort_keys=True, indent=4))
         # print (json_py.loads(r.text))
-        # print (r.json())
         # self.stderr.write(r.text)
-        self.stderr.write(str(r.json()))
-
-        result = json_py.loads(r.text)
-        # print ()
+        # self.stderr.write(str(r.json()))
         # print (result['data']['viewer']['repositories']['edges'][0])
         # print (result['data']['viewer']['repositories']['edges'][1])
-        # print ()
         # print (result['data']['viewer']['repositories']['totalCount'])
         # print (result['data']['viewer']['repositories']['edges'][0])
-
-        # self.stdout.write('')
-
         # print (result['data']['viewer']['repositories']['pageInfo']['endCursor'])
         # print (result['data']['viewer']['repositories']['pageInfo']['hasNextPage'])
+
+        result = json_py.loads(r.text)
 
         repo_list = []
         total_count = result['data']['viewer']['repositories']['totalCount']
 
         for repo_num in range(total_count):
-            self.stdout.write(str(repo_num))
+            # self.stdout.write(str(repo_num))
 
             current_repo = result['data']['viewer']['repositories']['edges'][repo_num]
 
-            self.stdout.write(str(current_repo))
-            self.stdout.write(current_repo['node']['name'])
+            # self.stdout.write(str(current_repo))
+            # self.stdout.write(current_repo['node']['name'])
 
             name = current_repo['node']['name']
 
@@ -66,27 +60,33 @@ class Command(BaseCommand):
 
             dt = current_repo['node']['pushedAt']
             pushed_at = datetime.strptime(dt, '%Y-%m-%dT%H:%M:%SZ')
-            self.stdout.write(str(pushed_at))
+            pushed_at = timezone.make_aware(pushed_at)
+            # self.stdout.write(str(pushed_at))
 
             url = current_repo['node']['url']
 
             repo_list.append(name)
 
             if Repository.objects.filter(repo_name=name):
-                self.stdout.write('check')
+                # self.stdout.write('check')
                 repo = Repository.objects.get(repo_name=name)
 
                 # if it is, check pushed_at. If database time is older, then update fields. Check for differences first????
+                # self.stdout.write(str(repo.pushed_at))
+                # self.stdout.write(str(pushed_at))
                 if repo.pushed_at < pushed_at:
-                    repo.update(description=description, pushed_at=pushed_at, url=url)
+                    repo.description = description
+                    repo.pushed_at = pushed_at
+                    repo.url = url
                     repo.save()
             else:
-                self.stdout.write('add')
+                # self.stdout.write('add')
                 new_entry = Repository(repo_name=name, description=description, pushed_at=pushed_at, url=url)
                 new_entry.save()
 
         for repo in Repository.objects.all():
             if repo.repo_name not in repo_list:
+                # self.stdout.write('delete: ' + str(repo.repo_name))
                 Repository.objects.get(repo_name=name).delete()
 
 
