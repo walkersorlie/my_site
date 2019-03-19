3e256c285770deb40a611043cae9c1ad325f8313
Walker Sorlie <walkersorlie@gmail.com>
2019-03-19 17:56:35 +0100

    Heroku

--- my_site/my_site/settings/base_settings.py
+++ my_site/my_site/settings/base_settings.py
@@ -16,6 +16,8 @@ import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+print('base dir:', BASE_DIR)
 
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
